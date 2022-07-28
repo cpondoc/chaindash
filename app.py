8d3fd880aa6c3d0c99db772d868a7bb486b3531a
cpondoc <chris.pondoc@comcast.net>
2022-07-28 15:32:23 -0700

    a ton of cosmetic updates

--- app.py
+++ app.py
@@ -12,12 +12,30 @@ import sqlite3
 app = Flask(__name__, instance_relative_config=True)
 database = 'db/sample_data.db'
 
-# Template -- have some data here
+# Template -- have some data her (will move to 'chains.toml')
 all_chains = ['avalanche', 'binance-smart-chain', 'polygon']
-descriptions = {
-    "avalanche": "Avalanche is an open-source platform for launching decentralized finance applications and enterprise blockchain deployments in one interoperable, scalable ecosystem. Developers who build on Avalanche can create applications and custom blockchain networks with complex rulesets or build on existing private or public subnets.",
-    "binance-smart-chain": "BNB Smart Chain (BSC) (Previously Binance Smart Chain) - EVM compatible, consensus layers, and with hubs to multi-chains.",
-    "polygon": "Polygon is a platform design to support infrastructure development and help Ethereum scale. Its core component is a modular, flexible framework (Polygon SDK) that allows developers to build and connect Layer-2 infrastructures like Plasma, Optimistic Rollups, zkRollups, and Validium and standalone sidechains like the project's flagship product, Matic POS (Proof-of-Stake).",
+chain_meta = {
+    'avalanche': {
+        'name': "Avalanche",
+        'description': "Avalanche is an open-source platform for launching decentralized finance applications and enterprise blockchain deployments in one interoperable, scalable ecosystem. Developers who build on Avalanche can create applications and custom blockchain networks with complex rulesets or build on existing private or public subnets.",
+        'website': "https://www.avax.network/",
+        'twitter': "https://twitter.com/avalancheavax",
+        'github': "https://github.com/ava-labs"
+    },
+    'binance-smart-chain': {
+        'name': "Binance Smart Chain",
+        'description': "BNB Smart Chain (BSC) (Previously Binance Smart Chain) - EVM compatible, consensus layers, and with hubs to multi-chains.",
+        'website': "https://www.binance.com/en",
+        'twitter': "https://twitter.com/BNBChain",
+        'github': "https://github.com/bnb-chain/bsc"
+    },
+    'polygon': {
+        'name': "Polygon",
+        'description': "Polygon is a platform design to support infrastructure development and help Ethereum scale. Its core component is a modular, flexible framework (Polygon SDK) that allows developers to build and connect Layer-2 infrastructures like Plasma, Optimistic Rollups, zkRollups, and Validium and standalone sidechains like the project's flagship product, Matic POS (Proof-of-Stake).",
+        'website': "https://polygon.technology/",
+        'twitter': "https://twitter.com/0xPolygon",
+        'github': "https://github.com/maticnetwork/"
+    }
 }
 
 '''
@@ -41,7 +59,7 @@ Starting endpoint (for right now, just point to developers)
 '''
 @app.route('/')
 def index():
-    return render_template('index.html', chains=all_chains, descriptions=descriptions)
+    return render_template('index.html', chains=all_chains, descriptions=chain_meta)
 
 '''
 Endpoint for visualizing data activity for each chain
@@ -65,8 +83,8 @@ def chain_devs(chain):
     acc_date_data, acc_data = get_daily_accs(chain, "Date(UTC)", "Unique Address Total Count")
 
     conn.close()
-    return render_template('chain.html', repositories=repos[:15],
-        description=descriptions[chain],
+    return render_template('chain.html', name=chain, repositories=repos[:20],
+        metadata=chain_meta,
         chain=chain_data, tvl_nums=tvl_data, tvl_dates=date_data,
         tx_nums=tx_data, tx_dates=tx_date_data, acc_nums=acc_data,
         acc_dates=acc_date_data)
